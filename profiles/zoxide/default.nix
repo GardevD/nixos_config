f5795a4b8cef2eae89660e067460b88a8e7a46f7
0rangeBlaze <gardevdaniel@gmail.com>
2024-07-25 19:50:17 +0200

    zoxide fix

--- profiles/zoxide/default.nix
+++ profiles/zoxide/default.nix
@@ -1,21 +1,10 @@
 {pkgs, ...}:
 {
-  nixpkgs.config.allowUnfree = true;
-  programs.bash = {
+  programs.zoxide = {
     enable = true;
-    historyIgnore = ["ls" "cd"];
-    shellAliases = {
-        nr = "nix run";
-        nd = "nix develop";
-        update-system = "sudo nixos-rebuild switch  --flake /etc/nixos#default --upgrade";
-    };
-    sessionVariables = {
-        XDG_DATA_HOME="$HOME/.local/share";
-    };
-    initExtra = ''
-        mkdir -p ~/.local/share/applications
-        sed 's/^Exec=/&nvidia-offload /' ${pkgs.discord}/share/applications/discord.desktop > ~/.local/share/applications/discord.desktop
-        sed 's/^Exec=/&nvidia-offload /' ${pkgs.vivaldi}/share/applications/vivaldi-stable.desktop > ~/.local/share/applications/vivaldi-stable.desktop
-    '';
+    enableBashIntegration = true;
+    options = [
+      "--cmd cd"
+    ];
   };
 }
