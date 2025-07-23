77801be5e48e205c2137b0d890568eb013f0f44e
0rangeBlaze <gardevdaniel@gmail.com>
2025-07-23 17:44:29 +0200

    avahi

--- hosts/g15/default.nix
+++ hosts/g15/default.nix
@@ -148,7 +148,11 @@ flake-overlays:
   )
     ];
 
-
+  # for resolving rpi5.local durin ssh
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+  };
 
 
   # Some programs need SUID wrappers, can be configured further or are
