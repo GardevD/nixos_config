872a4c014991d04ab14304245ad3c834e942b18d
0rangeBlaze <gardevdaniel@gmail.com>
2025-07-29 10:39:47 +0200

    experiment with vectorchord

--- hosts/rpi5/default.nix
+++ hosts/rpi5/default.nix
@@ -23,8 +23,40 @@
       workstation = true;
     };
   };
-
+  
   /*
+  nixpkgs.overlays = [
+    (self: super:
+      let
+        pkgsUnstable = import inputs.nixpkgs { system = "aarch64-linux"; };
+      in
+      {
+        vectorchord = pkgsUnstable.postgresql16Packages.vectorchord;
+      }
+    )
+  ];
+
+  #environment.systemPackages = [ pkgs.vectorchord ];
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_16.withPackages (ps: [ ps.pgvector pkgs.vectorchord ]);
+
+    ensureDatabases = [ "immich" ];
+      ensureUsers = [
+        {
+          name = "immich";
+          ensureDBOwnership = true;
+          ensureClauses.login = true;
+        }
+      ];
+      extensions = ps: [ ps.pgvector pkgs.vectorchord ];
+      settings = {
+        shared_preload_libraries = [ "vchord.so" ];
+        search_path = "\"$user\", public, vectors";
+      };
+
+  };
+
   services.immich = {
     enable = true;
     database.enable = false;
