33c000412d3417ad31245b8553af568e120baf22
0rangeBlaze <gardevdaniel@gmail.com>
2026-02-10 08:35:38 +0100

    removing some gnome packages

--- hosts/nixosModules/gnome.nix
+++ hosts/nixosModules/gnome.nix
@@ -1,7 +1,9 @@
-{ lib, ... }: {
+{ pkgs, lib, ... }: {
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs; [ epiphany geary gnome-contacts gnome-maps gnome-music gnome-weather gnome-connections ];
+
   services.gnome = {
     gnome-keyring.enable = lib.mkForce false;
     gnome-initial-setup.enable = false;
